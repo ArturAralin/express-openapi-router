c0bb5a7502d07dfec418629f1a106041b1c3a00a
Artur Aralin <artur.aralin97@gmail.com>
2020-05-21 20:52:27 +0300

    add router test

--- tests/router.spec.ts
+++ tests/router.spec.ts
@@ -1,70 +1,24 @@
 import {
   AORouter,
-  AOHandlerOptions,
-  OAHandlers,
 } from '../index';
 import express from 'express';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import sinon from 'sinon';
-import * as fs from 'fs';
-import * as path from 'path'
 import 'mocha';
 
 chai.use(chaiHttp);
 
-const OPEN_API_EXAMPLE_PATH = path.resolve(__dirname, './resources/open-api.example.json');
-const openApi = JSON.parse(fs.readFileSync(OPEN_API_EXAMPLE_PATH, 'utf-8'));
-
-describe('Controller', () => {
-  let app: express.Express;
-  let handlers: OAHandlers;
-
-  beforeEach(() => {
-    handlers = {
-      testHandler(params: AOHandlerOptions) {
-        params.res.sendStatus(200);
-      },
-    };
-
-    sinon.spy(handlers, 'testHandler');
-
+describe('Router', () => {
+  it('should be instance of Express.Router', () => {
     const router = AORouter({
       openApi: {
         openapi: '3.0.3',
         info: {},
-        paths: {
-          '/test': {
-            get: {
-              operationId: 'testHandler',
-            }
-          }
-        }
+        paths: {}
       },
-      handlers,
+      handlers: {},
     });
 
-    app = express();
-    app.use(router);
-  });
-
-  it('Params should contains all required fields', () => {
-    chai
-      .request(app)
-      .get('/test')
-      .end((err, res) => {
-        const { args: [[params]] } = handlers.testHandler as sinon.SinonSpy;
-        chai.expect(params.httpOk).is.a('function');
-        chai.expect(params.notFound).is.a('function');
-        chai.expect(params.internalError).is.a('function');
-        chai.expect(params.badRequest).is.a('function');
-        chai.expect(params.payload).is.a('object');
-        // ????
-        // chai.expect(params.payload.body).is.a('');
-        chai.expect(params.payload.params).is.a('object');
-        chai.expect(params.payload.query).is.a('object');
-        chai.expect(params.headers).is.a('object');
-        chai.expect(res.status).equal(200);
-      });
+    chai.expect(Object.getPrototypeOf(router) === express.Router).to.equals(true);
   });
 });

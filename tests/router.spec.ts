b6205dd8613907b51940efa536fc548356107006
Artur Aralin <artur.aralin97@gmail.com>
2020-05-23 12:21:54 +0300

    add router test

--- tests/router.spec.ts
+++ tests/router.spec.ts
@@ -6,6 +6,8 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import 'mocha';
 
+import { createApp } from './tools';
+
 chai.use(chaiHttp);
 
 describe('Router', () => {
@@ -39,5 +41,38 @@ describe('Router', () => {
       },
       handlers: {},
     });
+  });
+
+  it('should response unimplemented handler with HTTP 500', () => {
+    const app = createApp(AORouter({
+      responseFns: {
+        httpOk: (res, data) => {
+          res.json({
+            status: 'ok',
+            data,
+          });
+        }
+      },
+      openApi: {
+        openapi: '3.0.3',
+        info: {},
+        paths: {
+          '/nonexists': {
+            get: {
+              operationId: 'non_exists_handler',
+            }
+          }
+        }
+      },
+      handlers: {},
+    }));
+
+    chai
+      .request(app)
+      .get('/nonexists')
+      .end((_, res) => {
+        chai.expect(res.status).to.equals(500);
+        chai.expect(res.text).to.equals('Unimplemented GET /nonexists');
+      });
   });
 });
